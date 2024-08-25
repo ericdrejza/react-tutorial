400b8180ec4ece5246ec8dd3d832b4852a5bb0fe
Eric Drejza <edrejza@udel.edu>
2024-08-24 20:58:40 -0400

    Complete Lesson 5 - Click Events

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const Content = () => {
+  const handleClick = () => {
+    console.log('You clicked it');
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
       <p>Content</p>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2('Eric')}>Click It</button>
+      <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   );
 };
