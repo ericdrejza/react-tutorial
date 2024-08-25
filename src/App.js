c99627642e82315689a8174f7d9173ada2a9b439
Eric Drejza <edrejza@udel.edu>
2024-08-25 01:11:18 -0400

    Complete Lesson 11 - useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './Header';
 import SearchItem from './SearchItem';
@@ -8,15 +8,14 @@ import Footer from './Footer';
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('shoppingList'))
+    JSON.parse(localStorage.getItem('shoppingList')) || []
   );
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem('shoppingList', JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem('shoppingList', JSON.stringify(items));
+  }, [items]);
 
   const addItem = (item) => {
     const newItem = {
@@ -25,19 +24,19 @@ function App() {
       item: item
     };
     const listItems = [...items, newItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => id !== item.id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
